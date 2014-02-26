5ab42540239a8bc8ae45205109d72a3f8d2f70db
supraja <suprajasankari@gmail.com>
2014-02-25 21:39:42 -0600

    Added code to merge two sorted linked lists

--- LinkedList/LinkedList/PrintReverse.java
+++ LinkedList/LinkedList/PrintReverse.java
@@ -21,13 +21,32 @@ public class PrintReverse {
 
 	}
 	
+	private static LLNode merge(LLNode head1, LLNode head2) {
+	
+	        if(head1 == null) return head2;
+	        else if(head2 == null) return head1;
+	
+	        if(head1.value < head2.value) {
+	            head1.next = merge(head1.next, head2);
+	            return head1;
+	        }
+	        else {
+	            head2.next = merge(head2.next, head1);
+	            return head2;
+	        }
+	}
+	
 	public static void main(String[] args) {
 		
-		LLNode head = new LLNode(10);
-		head.next = new LLNode(13);
-		head.next.next = new LLNode(8);
-		head.next.next.next = new LLNode(1);
+		LLNode head = new LLNode(2);
+		head.next = new LLNode(4);
+		head.next.next = new LLNode(6);
+		head.next.next.next = new LLNode(8);
 		printReverse(head);
+		
+		LLNode head1 = new LLNode(1);
+		head1.next = new LLnode(3);
+		merge(head, head1);
 		
 	}
 
